0578359b5f2a99730ad5dcc2825a365b8cc6a4fe
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-02-10 18:58:05 +0000

    Command manager for server sided commands.

---
+++ src/server/managers/ui/command.ts
@@ -0,0 +1,30 @@
+import {Command} from "../../models/ui/chat/command";
+import {Server} from "../../server";
+import {Player} from "../../models/database/player";
+import {Events} from "../../../shared/enums/events";
+import {Suggestion} from "../../../shared/models/ui/chat/suggestion";
+
+export class CommandManager {
+  private server: Server;
+  private registeredCommands: Command[] = [];
+
+  constructor(server: Server) {
+    this.server = server;
+  }
+
+  // Get Methods
+  public get Commands(): Command[] {
+    return this.registeredCommands;
+  }
+
+  // Methods
+  public addCommand(command: Command): void {
+    this.registeredCommands.push(command);
+  }
+
+  public createChatSuggestions(player: Player): void {
+    this.registeredCommands.forEach(command => {
+      command.argsRequired ? player.TriggerEvent(Events.addSuggestion, new Suggestion(command.name, command.description, command.args)) : player.TriggerEvent(Events.addSuggestion, new Suggestion(command.name, command.description));
+    });
+  }
+}
