144c8dc1949c17902eef284023f1173b875e7560
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-03-08 21:40:52 +0000

    Forgot to push LX events enum.

---
+++ src/shared/enums/events/lxEvents.ts
@@ -0,0 +1,14 @@
+export enum LXEvents {
+  PedDied = "LX_Events:client:PedDied",
+  PedInjured = "LX_Events:client:PedInjured",
+  VehDamaged = "LX_Events:client:VehDamaged",
+  ObjDamaged = "LX_Events:client:ObjDamaged",
+  VehCollision = "LX_Events:client:vehCollision",
+  Gunshot = "LX_Events:client:gunshot",
+  EnteredVeh = "LX_Events:client:enteredVeh",
+  EnteringVeh = "LX_Events:client:enteringVehicle",
+  EnteringVehAborted = "LX_Events:client:enteringVehAborted",
+  LeftVeh = "LX_Events:client:leftVehicle",
+  PlayerDied = "LX_Events:client:playerDied",
+  PlayerKill = "LX_Events:client:playerKilled"
+}
