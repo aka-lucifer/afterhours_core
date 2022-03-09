d820786832ac6fbdb99d3d9e7ac6c638e37b1725
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-03-09 20:58:44 +0000

    Weather console commands and logging.

--- src/server/managers/sync/weather.ts
+++ src/server/managers/sync/weather.ts
@@ -1,7 +1,11 @@
 import {Server} from "../../server";
-import {enumMatches, randomEnum} from "../../utils";
+import {Capitalize, enumMatches, Error, Inform, randomEnum} from "../../utils";
 
+import {Player} from "../../models/database/player";
 import {Command} from "../../models/ui/chat/command";
+import WebhookMessage from "../../models/webhook/discord/webhookMessage";
+
+import { LogTypes } from "../../enums/logTypes";
 
 import {Weathers, WinterWeathers} from "../../../shared/enums/sync/weather";
 import {Events} from "../../../shared/enums/events/events";
@@ -10,7 +14,8 @@ import {SystemTypes} from "../../../shared/enums/ui/types";
 import {Ranks} from "../../../shared/enums/ranks";
 import {NotificationTypes} from "../../../shared/enums/ui/notifications/types";
 import serverConfig from "../../../configs/server.json";
-import {Player} from "../../models/database/player";
+import { EmbedColours } from "../../../shared/enums/embedColours";
+import sharedConfig from "../../../configs/shared.json";
 
 export class WeatherManager {
   private server: Server;
@@ -41,15 +46,34 @@ export class WeatherManager {
     GlobalState.weatherChanging = newState;
   }
 
-  private async setWeather(newWeather: string, manually: boolean): Promise<void> {
+  private async setWeather(newWeather: string, manually: boolean, changedBy?: Player): Promise<void> {
     this.currentWeather = newWeather;
     GlobalState.Weather = newWeather;
 
     if (manually) {
+      emitNet(Events.syncWeather, -1, this.currentWeather); // Sync weather to all clients
       this.setChanging(true);
+
       setTimeout(() => {
         this.setChanging(false);
       }, serverConfig.syncing.time.secondInterval);
+
+      if (changedBy !== undefined) {
+        const changersDisc = await changedBy.GetIdentifier("discord");
+        await this.server.logManager.Send(LogTypes.Action, new WebhookMessage({username: "Character Logs", embeds: [{
+          color: EmbedColours.Green,
+          title: "__Weather Changing__",
+          description: `The weather has been changed.\n\n**Weather**: ${Capitalize(newWeather.toLowerCase())}\n**Changed By**: ${changedBy.GetName}\n**Rank**: ${Ranks[changedBy.GetRank]}\n**Discord**: ${changersDisc != "Unknown" ? `<@${changersDisc}>` : changersDisc}`,
+          footer: {text: `${sharedConfig.serverName} - ${new Date().toUTCString()}`, icon_url: sharedConfig.serverLogo}
+        }]}));
+      } else {
+        await this.server.logManager.Send(LogTypes.Action, new WebhookMessage({username: "Character Logs", embeds: [{
+          color: EmbedColours.Green,
+          title: "__Weather Changing__",
+          description: `The weather has been changed.\n\n**Weather**: ${Capitalize(newWeather.toLowerCase())}\n**Changed By**: Console`,
+          footer: {text: `${sharedConfig.serverName} - ${new Date().toUTCString()}`, icon_url: sharedConfig.serverLogo}
+        }]}));
+      }
     }
   }
 
@@ -81,8 +105,7 @@ export class WeatherManager {
             if (!this.winterWeather) {
               const [type, matches] = await enumMatches(Weathers, weatherType)
               if (matches) {
-                await this.setWeather(weatherType, true);
-                emitNet(Events.syncWeather, -1, this.currentWeather);
+                await this.setWeather(weatherType, true, player);
                 await player.Notify("Weather", `Changing weather to ${type}`, NotificationTypes.Success);
               } else {
                 await player.TriggerEvent(Events.sendSystemMessage, new Message("Entered weather type doesn't exist!", SystemTypes.Error));
@@ -90,8 +113,7 @@ export class WeatherManager {
             } else {
               const [type, matches] = await enumMatches(WinterWeathers, weatherType)
               if (matches) {
-                await this.setWeather(weatherType, true);
-                emitNet(Events.syncWeather, -1, this.currentWeather);
+                await this.setWeather(weatherType, true, player);
                 await player.Notify("Weather", `Changing weather to ${type}`, NotificationTypes.Success);
               } else {
                 await player.TriggerEvent(Events.sendSystemMessage, new Message("Entered weather type doesn't exist!", SystemTypes.Error));
@@ -101,7 +123,7 @@ export class WeatherManager {
             await player.TriggerEvent(Events.sendSystemMessage, new Message("You have entered a number, not a weather type!", SystemTypes.Error));
           }
         } else {
-          await player.TriggerEvent(Events.sendSystemMessage, new Message("You can't change the servers weather, as the weather is frozen!", SystemTypes.Error));
+          await player.TriggerEvent(Events.sendSystemMessage, new Message("You can't change the server's weather, as the weather is frozen!", SystemTypes.Error));
         }
       } else {
         await player.TriggerEvent(Events.sendSystemMessage, new Message("Server weather is already changing!", SystemTypes.Error));
@@ -111,13 +133,60 @@ export class WeatherManager {
     new Command("freezeweather", "Freeze the weather", [], false, async(source: string) => {
       const player = await this.server.connectedPlayerManager.GetPlayer(source);
       this.setFrozen(!this.weatherFrozen);
-      // emitNet(Events.freezeWeather, -1);
       if (this.weatherFrozen) {
         await player.Notify("Sync Manager", "You've frozen weather!", NotificationTypes.Success);
       } else {
         await player.Notify("Sync Manager", "You've unfrozen weather!", NotificationTypes.Success);
       }
     }, Ranks.Admin);
+
+    // RCON Commands
+    RegisterCommand("weather", async(source: string, args: any[]) => {
+      if (parseInt(source) <= 0) {
+        if (!this.weatherChanging) {
+          if (!this.weatherFrozen) {
+            if (isNaN(args[0])) {
+              const weatherType = args[0].toUpperCase();
+  
+              if (!this.winterWeather) {
+                const [type, matches] = await enumMatches(Weathers, weatherType)
+                if (matches) {
+                  await this.setWeather(weatherType, true);
+                  Inform("Weather Manager", `Changing weather to ${type}`);
+                } else {
+                  Error("Weather Manager", "Entered weather type doesn't exist!");
+                }
+              } else {
+                const [type, matches] = await enumMatches(WinterWeathers, weatherType)
+                if (matches) {
+                  await this.setWeather(weatherType, true);
+                  Inform("Weather Manager", `Changing weather to ${type}`);
+                } else {
+                  Error("Weather Manager", "Entered weather type doesn't exist!");
+                }
+              }
+            } else {
+              Error("Weather Manager", "You have entered a number, not a weather type!");
+            }
+          } else {
+            Inform("Weather Manager", "You can't change the server's weather, as the weather is frozen!");
+          }
+        } else {
+          Inform("Weather Manager", "Server weather is already changing!");
+        }
+      }
+    }, false);
+    
+    RegisterCommand("freezeweather", async(source: string) => {
+      if (parseInt(source) <= 0) {
+        this.setFrozen(!this.weatherFrozen);
+        if (this.weatherFrozen) {
+          Inform("Weather Manager", `Weather frozen`);
+        } else {
+          Inform("Weather Manager", `Weather unfrozen`);
+        }
+      }
+    }, false);
   }
 
   public async sync(player: Player): Promise<void> {
