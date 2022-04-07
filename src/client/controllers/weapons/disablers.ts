475ec3f738bf59889e08bcb09ace78b5b4cd3192
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-04-07 20:59:46 +0100

    Punch spamming disabler.

--- src/client/controllers/weapons/disablers.ts
+++ src/client/controllers/weapons/disablers.ts
@@ -5,19 +5,25 @@ import { Delay } from "../../utils";
 import { Events } from "../../../shared/enums/events/events";
 import { Weapons } from "../../../shared/enums/weapons";
 
+import clientConfig from "../../../configs/client.json";
+
 export class WeaponDisablers {
   private rollTick: number = undefined;
+  private punchTick: number = undefined;
+  private punchControlTick: number = undefined;
   private stanceFixerTick: number = undefined;
 
   constructor() {
-
-    // Events
-    onNet(Events.gameEventTriggered, this.gameEvent.bind(this));
+    console.log("started!");
   }
 
   // Getters
   public get RollActive(): boolean {
     return this.rollTick !== undefined;
+  }
+
+  public get PunchActive(): boolean {
+    return this.punchTick !== undefined;
   }
 
   // Methods
@@ -29,6 +35,8 @@ export class WeaponDisablers {
         await Delay(500);
       }
     });
+
+    this.startPunch(); // Have to start it manually, however it will be managed by "removers.ts"
   }
 
   public startRoll(): void {
@@ -49,18 +57,46 @@ export class WeaponDisablers {
     }
   }
 
-  // Events
-  private async gameEvent(eventName: string, eventArgs: any[]): Promise<void> {
-    if (eventName == "CEventNetworkEntityDamage") {
-      const attackingEntity = new Ped(eventArgs[0]);
-      if (attackingEntity.IsPlayer) {
-        const isMelee = eventArgs[11];
-        if (isMelee) {
-          const weaponHash = eventArgs[6];
-          if (weaponHash == Weapons.Unarmed) {
-            console.log("person punched you!");
-          }
+  public startPunch(): void { // Will use 0.02ms
+    // Disable Punch Spammer
+    if (this.punchTick === undefined) this.punchTick = setTick(async() => {
+      const myPed = Game.PlayerPed;
+      // If we're unarmed
+      if (GetSelectedPedWeapon(myPed.Handle) == Weapons.Unarmed) {
+        // If we have punched someone
+        if (IsPedPerformingMeleeAction(myPed.Handle)) { // If punched
+          if (this.punchControlTick === undefined) this.punchControlTick = setTick(() => {
+            Game.disableControlThisFrame(InputMode.MouseAndKeyboard, Control.Attack);
+            Game.disableControlThisFrame(InputMode.MouseAndKeyboard, Control.Attack2);
+            Game.disableControlThisFrame(InputMode.MouseAndKeyboard, Control.MeleeAttack1);
+            Game.disableControlThisFrame(InputMode.MouseAndKeyboard, Control.MeleeAttack2);
+            Game.disableControlThisFrame(InputMode.MouseAndKeyboard, Control.MeleeAttackAlternate);
+            Game.disableControlThisFrame(InputMode.MouseAndKeyboard, Control.MeleeAttackHeavy);
+            Game.disableControlThisFrame(InputMode.MouseAndKeyboard, Control.MeleeAttackLight);
+            Game.disableControlThisFrame(InputMode.MouseAndKeyboard, Control.MeleeBlock);
+          });
+          
+          await Delay(clientConfig.controllers.weapons.disablers.antiPunch.time);
+
+          clearTick(this.punchControlTick);
+          this.punchControlTick = undefined;
+        } else {
+          await Delay(500);
         }
+      } else {
+        await Delay(500);
+      }
+    });
+  }
+
+  public stopPunch(): void {
+    if (this.punchTick !== undefined) {
+      clearTick(this.punchTick);
+      this.punchTick = undefined;
+
+      if (this.punchControlTick !== undefined) {
+        clearTick(this.punchControlTick);
+        this.punchControlTick = undefined;
       }
     }
   }
