ad93120a3ede36e83fd568c1ae362c9d7ea9e396
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-04-09 20:03:04 +0100

    Repair Shops

---
+++ src/client/controllers/vehicles/repairShops.ts
@@ -0,0 +1,114 @@
+import { BlipSprite, Control, Game, InputMode, Screen, Vector3, VehicleDoorIndex, World } from "fivem-js";
+
+import { PolyZone } from "../../helpers/PolyZone";
+import { BoxZone } from "../../helpers/boxZone";
+
+import { Progress } from "../../models/ui/progress";
+import { Notification } from "../../models/ui/notification";
+
+import { Sounds } from "../../../shared/enums/sounds";
+
+import clientConfig from "../../../configs/client.json";
+import { NotificationTypes } from "../../../shared/enums/ui/notifications/types";
+
+export class RepairShops {
+  private shops: PolyZone[] = [];
+  private showNotify: boolean = false;
+  private repairing: boolean = false;
+  private tick: number = undefined;
+
+  constructor() {
+    console.log("repair shops");
+  }
+
+  public init(): void {
+    const shops = clientConfig.controllers.vehicles.repairing.shopLocations;
+
+    for (let i = 0; i < shops.length; i++) {
+      const blipPos = new Vector3(shops[i].location.x, shops[i].location.y, shops[i].location.z)
+      const blip = World.createBlip(blipPos);
+      blip.Sprite = 446;
+      blip.Scale = 0.7;
+      blip.IsShortRange = true;
+      blip.Name = "Repair Shop";
+
+      const zone = new BoxZone({
+        box: shops[i].location,
+        options: {
+          name: shops[i].name,
+          heading: 0,
+          debugPoly: false
+        }
+      }).create();
+
+      this.shops.push(zone);
+    }
+  }
+
+  private async interactTask(): Promise<void> {
+    if (!this.repairing) {
+      const myPed = Game.PlayerPed;
+      if (IsPedInAnyVehicle(myPed.Handle, false)) {
+        const currVeh = myPed.CurrentVehicle;
+
+        Screen.displayHelpTextThisFrame("~INPUT_PICKUP~ Repair Vehicle");
+        if (Game.isControlJustPressed(InputMode.MouseAndKeyboard, Control.Pickup)) {
+          if (currVeh.Health < currVeh.MaxHealth) {
+            const progress = new Progress(20000, {
+              vehicle: true
+            }, async() => {
+              if (global.exports["xsound"].isPlaying("veh_repair")) global.exports["xsound"].Destroy("veh_repair");
+              SetVehicleDoorShut(currVeh.Handle, VehicleDoorIndex.Hood, false)
+              currVeh.IsEngineRunning = true;
+              currVeh.IsDriveable = true;
+              SetVehicleEngineOn(currVeh.Handle, true, false, true);
+              SetVehicleUndriveable(currVeh.Handle, false);
+              this.repairing = false;
+              const notify = new Notification("Mechanic", "You cancelled repairing your vehicle!", NotificationTypes.Error);
+              await notify.send();
+            }, () => {
+              this.repairing = true;
+              SetVehicleDoorOpen(currVeh.Handle, VehicleDoorIndex.Hood, false, false);
+              currVeh.IsEngineRunning = false;
+              currVeh.IsDriveable = false;
+              global.exports["xsound"].PlayUrlPos("veh_repair", Sounds.Repair, 1, Game.PlayerPed.Position, false);
+            }, async() => {
+              if (global.exports["xsound"].isPlaying("veh_repair")) global.exports["xsound"].Destroy("veh_repair");
+              currVeh.repair();
+              SetVehicleDoorShut(currVeh.Handle, VehicleDoorIndex.Hood, false)
+              currVeh.IsEngineRunning = true;
+              currVeh.IsDriveable = true;
+              SetVehicleEngineOn(currVeh.Handle, true, false, true);
+              SetVehicleUndriveable(currVeh.Handle, false);
+              currVeh.DirtLevel = 0.0;
+              const notify = new Notification("Mechanic", "Your vehicle has been repaired", NotificationTypes.Info);
+              await notify.send();
+              this.repairing = false;
+            })
+            
+            progress.start();
+          } else {
+            const notify = new Notification("Mechanic", "Your vehicle isn't damaged!", NotificationTypes.Error);
+            await notify.send();
+          }
+        }
+      }
+    }
+  }
+
+  public start(): void {
+    for (let i = 0; i < this.shops.length; i++) {
+      this.shops[i].onPlayerInOut(async(isInside: boolean, pedPos: Vector3) => {
+        if (isInside) {
+          if (this.tick === undefined) this.tick = setTick(this.interactTask);
+        } else {
+          if (this.tick !== undefined) {
+            clearTick(this.tick);
+            this.tick = undefined;
+            console.log("cleared tick!");
+          }
+        }
+      }, 1000);
+    }
+  }
+}
