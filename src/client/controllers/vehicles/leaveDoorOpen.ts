1cb7ee2a82266fe5151bbe4199dfa44d782c3d29
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-04-09 17:17:01 +0100

    Leave door open for LEO vehicles.

---
+++ src/client/controllers/vehicles/leaveDoorOpen.ts
@@ -0,0 +1,45 @@
+import { Control, Game, InputMode, VehicleClass } from "fivem-js";
+
+import { Delay } from "../../utils";
+
+export class LeaveDoorOpen {
+  private tick: number = undefined;
+
+  constructor() {
+    console.log("started door open!");
+  }
+
+  // Getters
+  public get Started(): boolean {
+    return this.tick !== undefined;
+  }
+
+  // Methods
+  public start(): void {
+    console.log("start leave door open!");
+    if (this.tick === undefined) this.tick = setTick(async() => {
+      const myPed = Game.PlayerPed;
+      if (IsPedInAnyVehicle(myPed.Handle, false)) {
+        const currVeh = myPed.CurrentVehicle;
+        if (currVeh.ClassType == VehicleClass.Emergency) {
+          if (Game.isControlPressed(InputMode.MouseAndKeyboard, Control.Enter)) {
+            currVeh.IsEngineRunning = true;
+            TaskLeaveVehicle(myPed.Handle, currVeh.Handle, 256);
+          }
+        } else {
+          await Delay(500);
+        }
+      } else {
+        await Delay(500);
+      }
+    });
+  }
+
+  public stop(): void {
+    if (this.tick !== undefined) {
+      console.log("stop leave door open!");
+      clearTick(this.tick);
+      this.tick = undefined;
+    }
+  }
+}
