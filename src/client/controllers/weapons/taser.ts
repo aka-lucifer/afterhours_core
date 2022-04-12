ead359e6ceb9562b56f9ca394da36ddafb0172b9
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-04-12 15:34:49 +0100

    Taser laser sight is now accurate in 3rd person.

--- src/client/controllers/weapons/taser.ts
+++ src/client/controllers/weapons/taser.ts
@@ -223,8 +223,18 @@ export class Taser {
               const [hit, coords] = this.screenToWorld(weapon.Position, 15000, myPed);
 
               if (hit) {
-                World.drawLine(weapon.Position, coords, Color.fromRgb(clientConfig.controllers.weapons.taser.laser.colour.r, clientConfig.controllers.weapons.taser.laser.colour.g, clientConfig.controllers.weapons.taser.laser.colour.b));
-                World.drawMarker(MarkerType.DebugSphere, coords, new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0.01, 0.01, 0.01), Color.fromRgb(clientConfig.controllers.weapons.taser.laser.colour.r, clientConfig.controllers.weapons.taser.laser.colour.g, clientConfig.controllers.weapons.taser.laser.colour.b), false, false, false, null, null, false);
+                const camMode = GetFollowPedCamViewMode();
+
+                // If camera mode is first person
+                if (camMode == 4) {
+                  World.drawLine(weapon.Position, coords, Color.fromRgb(clientConfig.controllers.weapons.taser.laser.colour.r, clientConfig.controllers.weapons.taser.laser.colour.g, clientConfig.controllers.weapons.taser.laser.colour.b));
+                  World.drawMarker(MarkerType.DebugSphere, coords, new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0.01, 0.01, 0.01), Color.fromRgb(clientConfig.controllers.weapons.taser.laser.colour.r, clientConfig.controllers.weapons.taser.laser.colour.g, clientConfig.controllers.weapons.taser.laser.colour.b), false, false, false, null, null, false);
+                } else { // If it's third person
+                  const newCoords = new Vector3(coords.x, coords.y + 0.22, coords.z); // Reposition the laser to match the ADS reticle position
+
+                  World.drawLine(weapon.Position, newCoords, Color.fromRgb(clientConfig.controllers.weapons.taser.laser.colour.r, clientConfig.controllers.weapons.taser.laser.colour.g, clientConfig.controllers.weapons.taser.laser.colour.b));
+                  World.drawMarker(MarkerType.DebugSphere, newCoords, new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0.01, 0.01, 0.01), Color.fromRgb(clientConfig.controllers.weapons.taser.laser.colour.r, clientConfig.controllers.weapons.taser.laser.colour.g, clientConfig.controllers.weapons.taser.laser.colour.b), false, false, false, null, null, false);
+                }
               }
             } else {
               await Delay(100);
