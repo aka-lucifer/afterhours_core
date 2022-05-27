e832c1b03eec9e85189c2de110ffb19e69c4793c
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-05-27 12:50:11 +0100

    Made cuff/uncuff commands into events.

--- src/server/controllers/jobs/police/cuffing.ts
+++ src/server/controllers/jobs/police/cuffing.ts
@@ -1,7 +1,7 @@
 import { Vector3 } from 'fivem-js';
 
 import { Server } from '../../../server';
-import { Delay, getClosestPlayer } from '../../../utils';
+import { Delay, Dist, getClosestPlayer, Inform, Log, NumToVector3 } from '../../../utils';
 
 import { ProximityTypes } from '../../../managers/characters';
 
@@ -10,6 +10,8 @@ import { CuffState } from '../../../../shared/enums/jobs/cuffStates';
 import { NotificationTypes } from '../../../../shared/enums/ui/notifications/types';
 import { Message } from '../../../../shared/models/ui/chat/message';
 import { SystemTypes } from '../../../../shared/enums/ui/chat/types';
+import { formatFirstName } from '../../../../shared/utils';
+import { Events } from '../../../../shared/enums/events/events';
 
 export class Cuffing {
   private server: Server;
@@ -19,101 +21,10 @@ export class Cuffing {
 
     // Events
     onNet(JobEvents.cuffPlayer, this.EVENT_cuffPlayer.bind(this))
+    onNet(JobEvents.uncuffPlayer, this.EVENT_uncuffPlayer.bind(this));
     onNet(JobEvents.doPerpBackAnim, this.EVENT_doPerpBackAnim.bind(this));
     onNet(JobEvents.doPerpFrontAnim, this.EVENT_doPerpFrontAnim.bind(this));
     onNet(JobEvents.setFinished, this.EVENT_setFinished.bind(this));
-
-    RegisterCommand("cuff", async(source: string) => {
-      const player = await this.server.connectedPlayerManager.GetPlayer(source);
-
-      if (player) {
-        if (player.Spawned) {
-          const character = await this.server.characterManager.Get(player);
-          if (character.isLeoJob()) { // If your selected character is an LEO
-            if (character.Job.Status) { // If your character is on duty
-              const [closest, dist] = await getClosestPlayer(player);
-
-              if (closest) {
-                if (dist < 3) {
-                  const closestCharacter = await this.server.characterManager.Get(closest);
-                  if (closestCharacter) {
-                    const perpStates = Player(closest.Handle);
-                    if (perpStates.state.cuffState == CuffState.Uncuffed) {
-                      console.log("cuff him!");
-                      await player.TriggerEvent(JobEvents.startCuffing, closest.Handle);
-                    } else if (perpStates.state.cuffState == CuffState.Shackled) {
-                      await player.Notify("Cuffing", "This person can't be cuffed, as they're already shackled!", NotificationTypes.Error);
-                    } else {
-                      await player.Notify("Cuffing", "This person is already cuffed!", NotificationTypes.Error);
-                    }
-                  } else {
-                    await player.Notify("Cuffing", "This person hasn't selected a character!", NotificationTypes.Error);
-                  }
-                } else {
-                  await player.Notify("Cuffing", "Player is too far away!", NotificationTypes.Error);
-                }
-              } else {
-                await player.Notify("Cuffing", "No one found!", NotificationTypes.Error);
-              }
-            } else {
-              await player.Notify("Cuffing", "You aren't on duty!", NotificationTypes.Error);
-            }
-          }
-        }
-      }
-    }, false);
-
-    RegisterCommand("uncuff", async(source: string) => {
-      const player = await this.server.connectedPlayerManager.GetPlayer(source);
-
-      if (player) {
-        if (player.Spawned) {
-          const character = await this.server.characterManager.Get(player);
-          if (character.isLeoJob()) { // If your selected character is an LEO
-            if (character.Job.Status) { // If your character is on duty
-              const [closest, dist] = await getClosestPlayer(player);
-
-              if (closest) {
-                if (dist < 3) {
-                  const closestCharacter = await this.server.characterManager.Get(closest);
-                  if (closestCharacter) {
-                    const perpStates = Player(closest.Handle);
-                    if (perpStates.state.cuffState == CuffState.Cuffed || perpStates.state.cuffState == CuffState.Shackled) {
-                      await closest.TriggerEvent(JobEvents.setGrabbed, player.Handle); // Attach them to us
-
-                      // Display progress UI for 2 seconds.
-                      await player.TriggerEvent(JobEvents.startUncuffing);
-                      await Delay(2000); // Wait 2 seconds
-
-                      await closest.TriggerEvent(JobEvents.stopGrabbing); // Detach them
-                      await closest.TriggerEvent(JobEvents.setUncuffed); // Uncuff them
-                      perpStates.state.cuffState = CuffState.Uncuffed; // Set them to uncuffed
-
-                      await closest.Notify("Cuffing", "You've been uncuffed", NotificationTypes.Success);
-
-                      const sent = await this.server.characterManager.proximityMessage(ProximityTypes.Me, new Message(`uncuffs ${closestCharacter.Name}`, SystemTypes.Me), character);
-                      if (sent) {
-                        await this.server.characterManager.meDrawing(parseInt(player.Handle), `uncuffs ${closestCharacter.Name}`);
-                      }
-                    } else {
-                      await player.Notify("Cuffing", "This person isn't cuffed!", NotificationTypes.Error);
-                    }
-                  } else {
-                    await player.Notify("Cuffing", "This person hasn't selected a character!", NotificationTypes.Error);
-                  }
-                } else {
-                  await player.Notify("Cuffing", "Player is too far away!", NotificationTypes.Error);
-                }
-              } else {
-                await player.Notify("Cuffing", "No one found!", NotificationTypes.Error);
-              }
-            } else {
-              await player.Notify("Cuffing", "You aren't on duty!", NotificationTypes.Error);
-            }
-          }
-        }
-      }
-    }, false);
   }
 
   // Events
@@ -126,17 +37,93 @@ export class Cuffing {
         if (character.isLeoJob()) { // If your selected character is an LEO
           if (character.Job.Status) { // If your character is on duty
             const [closest, dist] = await getClosestPlayer(player);
-            console.log("closest player", closest, dist)
-            if (dist < 3) {
-              const perpStates = Player(closest.Handle);
-              if (perpStates.state.cuffState == CuffState.Uncuffed) {
-                console.log("cuff him!");
-                await player.TriggerEvent(JobEvents.startCuffing, closest.Handle);
-              } else if (perpStates.state.cuffState == CuffState.Shackled) {
-                await player.Notify("Cuffing", "This person can't be cuffed, as they're already shackled!", NotificationTypes.Error);
+
+            if (closest) {
+              if (dist < 3) {
+                const closestCharacter = await this.server.characterManager.Get(closest);
+                if (closestCharacter) {
+                  const perpStates = Player(closest.Handle);
+                  if (perpStates.state.cuffState == CuffState.Uncuffed) {
+                    console.log("cuff him!");
+                    await player.TriggerEvent(JobEvents.startCuffing, closest.Handle);
+                  } else if (perpStates.state.cuffState == CuffState.Shackled) {
+                    await player.Notify("Cuffing", "This person can't be cuffed, as they're already shackled!", NotificationTypes.Error);
+                  } else {
+                    await player.Notify("Cuffing", "This person is already cuffed!", NotificationTypes.Error);
+                  }
+                } else {
+                  await player.Notify("Cuffing", "This person hasn't selected a character!", NotificationTypes.Error);
+                }
               } else {
-                await player.Notify("Cuffing", "This person is already cuffed!", NotificationTypes.Error);
+                await player.Notify("Cuffing", "Player is too far away!", NotificationTypes.Error);
               }
+            } else {
+              await player.Notify("Cuffing", "No one found!", NotificationTypes.Error);
+            }
+          } else {
+            await player.Notify("Cuffing", "You aren't on duty!", NotificationTypes.Error);
+          }
+        }
+      }
+    }
+  }
+
+  private async EVENT_uncuffPlayer(): Promise<void> {
+    const player = await this.server.connectedPlayerManager.GetPlayer(source);
+
+    if (player) {
+      if (player.Spawned) {
+        const character = await this.server.characterManager.Get(player);
+        if (character.isLeoJob()) { // If your selected character is an LEO
+          if (character.Job.Status) { // If your character is on duty
+            const [closest, dist] = await getClosestPlayer(player);
+
+            if (closest) {
+              if (dist < 3) {
+                const closestCharacter = await this.server.characterManager.Get(closest);
+                if (closestCharacter) {
+                  const perpStates = Player(closest.Handle);
+                  if (perpStates.state.cuffState == CuffState.Cuffed || perpStates.state.cuffState == CuffState.Shackled) {
+                    await closest.TriggerEvent(JobEvents.setGrabbed, player.Handle); // Attach them to us
+
+                    // Display progress UI for 2 seconds.
+                    await player.TriggerEvent(JobEvents.startUncuffing);
+                    await Delay(2000); // Wait 2 seconds
+
+                    await closest.TriggerEvent(JobEvents.stopGrabbing); // Detach them
+                    await closest.TriggerEvent(JobEvents.setUncuffed); // Uncuff them
+                    perpStates.state.cuffState = CuffState.Uncuffed; // Set them to uncuffed
+
+                    await closest.Notify("Cuffing", "You've been uncuffed", NotificationTypes.Success);
+
+                    // Send /me message below
+                    const svPlayers = this.server.connectedPlayerManager.GetPlayers;
+
+                    for (let i = 0; i < svPlayers.length; i++) {
+                      const otherPlayer = svPlayers[i];
+                      const dist = player.Position.distance(otherPlayer.Position);
+
+                      Log("Proximity Message", `My Position: ${JSON.stringify(player.Position)} | Other Position: ${JSON.stringify(otherPlayer.Position)} | Dist: ${dist}`);
+
+                      if (dist <= 60.0) {
+                        Inform("Proximity Message", `Player (${otherPlayer.GetName}) is close enough to recieve the proximity message sent from (${character.Owner.GetName})`);
+                        await otherPlayer.TriggerEvent(Events.sendSystemMessage, new Message(`uncuffs ${closestCharacter.Name}`, SystemTypes.Me), `[${character.Job.Callsign}] | ${formatFirstName(character.firstName)}. ${character.lastName}`);
+                      }
+                    }
+
+                    // Display 3D /me above your head
+                    await this.server.characterManager.meDrawing(parseInt(player.Handle), `handcuffs ${closestCharacter.Name}`);
+                  } else {
+                    await player.Notify("Cuffing", "This person isn't cuffed!", NotificationTypes.Error);
+                  }
+                } else {
+                  await player.Notify("Cuffing", "This person hasn't selected a character!", NotificationTypes.Error);
+                }
+              } else {
+                await player.Notify("Cuffing", "Player is too far away!", NotificationTypes.Error);
+              }
+            } else {
+              await player.Notify("Cuffing", "No one found!", NotificationTypes.Error);
             }
           } else {
             await player.Notify("Cuffing", "You aren't on duty!", NotificationTypes.Error);
@@ -199,10 +186,23 @@ export class Cuffing {
                 perpStates.state.set("cuffState", CuffState.Cuffed, true); // Set their state bag to cuffed
                 emitNet(JobEvents.setCuffed, netToArrest); // Start their cuff event (attach handcuff prop, disable controls, start tick)
 
-                const sent = await this.server.characterManager.proximityMessage(ProximityTypes.Me, new Message(`handcuffs ${perpsCharacter.Name}`, SystemTypes.Me), character);
-                if (sent) {
-                  await this.server.characterManager.meDrawing(parseInt(player.Handle), `handcuffs ${perpsCharacter.Name}`);
+                // Send /me message below
+                const svPlayers = this.server.connectedPlayerManager.GetPlayers;
+
+                for (let i = 0; i < svPlayers.length; i++) {
+                  const otherPlayer = svPlayers[i];
+                  const dist = player.Position.distance(otherPlayer.Position);
+
+                  Log("Proximity Message", `My Position: ${JSON.stringify(player.Position)} | Other Position: ${JSON.stringify(otherPlayer.Position)} | Dist: ${dist}`);
+
+                  if (dist <= 60.0) {
+                    Inform("Proximity Message", `Player (${otherPlayer.GetName}) is close enough to recieve the proximity message sent from (${character.Owner.GetName})`);
+                    await otherPlayer.TriggerEvent(Events.sendSystemMessage, new Message(`handcuffs ${perpsCharacter.Name}`, SystemTypes.Me), `[${character.Job.Callsign}] | ${formatFirstName(character.firstName)}. ${character.lastName}`);
+                  }
                 }
+
+                // Display 3D /me above your head
+                await this.server.characterManager.meDrawing(parseInt(player.Handle), `handcuffs ${perpsCharacter.Name}`);
               }
             }
           } else {
