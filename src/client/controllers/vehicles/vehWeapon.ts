a2643e0d0998da29707cea55f009e160747318ad
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-06-05 18:21:50 +0100

    Vehicle weapon prop now attaches to the R hand.

--- src/client/controllers/vehicles/vehWeapon.ts
+++ src/client/controllers/vehicles/vehWeapon.ts
@@ -65,9 +65,10 @@ export class VehicleWeapon {
                 this.attachedWeapon = await World.createProp(weaponModel, myPed.Position, false, false);
 
 
-                const rightHandVeh = await rightHandVehicle(currVeh);
-                const boneToAttach = !rightHandVeh ? Bone.SKEL_R_Hand : Bone.SKEL_L_Hand;
+                // const rightHandVeh = await rightHandVehicle(currVeh);
+                // const boneToAttach = !rightHandVeh ? Bone.SKEL_R_Hand : Bone.SKEL_L_Hand;
                 // console.log("vehs 1", rightHandVeh, boneToAttach);
+                const boneToAttach = Bone.SKEL_R_Hand
 
                 AttachEntityToEntity(this.attachedWeapon.Handle, myPed.Handle, GetPedBoneIndex(myPed.Handle, boneToAttach), 0.18, 0.035, -0.001, -82.2, -2.6449, -7.71, true, true, false, false, 1, true)
                 this.hasAttached = true;
