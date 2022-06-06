7f87528655e7c53495ddb933e2de66a32befd9ff
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-06-06 23:24:07 +0100

    Some drive by code improvement.

--- src/client/controllers/vehicles/driveBy.ts
+++ src/client/controllers/vehicles/driveBy.ts
@@ -41,25 +41,37 @@ export class DriveBy {
             if (vehSpeed > 30) {
               if (this.client.Player.Rank >= Ranks.SeniorAdmin) {
                 // console.log("snr admin or above!");
-                SetPlayerCanDoDriveBy(Game.Player.Handle, true); // Allow firing as we're senior admin, so we can be trusted
-                if (!this.canDriveBy) this.canDriveBy = true;
+                if (!this.canDriveBy) {
+                  SetPlayerCanDoDriveBy(Game.Player.Handle, true); // Allow firing as we're senior admin, so we can be trusted
+                  this.canDriveBy = true;
+                }
               } else {
                 if (this.client.Character.isLeoJob() && this.client.Character.job.status) {
                   // console.log("on duty LEO!");
-                  SetPlayerCanDoDriveBy(Game.Player.Handle, true); // Allow firing as we're on duty LEO
-                  if (!this.canDriveBy) this.canDriveBy = true;
+                  if (!this.canDriveBy) {
+                    SetPlayerCanDoDriveBy(Game.Player.Handle, true); // Allow firing as we're on duty LEO
+                    this.canDriveBy = true;
+                  }
                 } else {
                   // if (currWeapon !== Weapons.Unarmed) SetCurrentPedWeapon(myPed.Handle, Weapons.Unarmed, true);
-                  SetPlayerCanDoDriveBy(Game.Player.Handle, false); // Disable firing
-                  if (this.canDriveBy) this.canDriveBy = false;
+                  if (this.canDriveBy) {
+                    SetPlayerCanDoDriveBy(Game.Player.Handle, false); // Disable firing
+                    this.canDriveBy = false;
+                  }
                 }
               }
             } else {
-              SetPlayerCanDoDriveBy(Game.Player.Handle, true); // Set this here, so when we go under 30mph, we can fire again
-              if (!this.canDriveBy) this.canDriveBy = true;
+              if (!this.canDriveBy) {
+                SetPlayerCanDoDriveBy(Game.Player.Handle, true); // Set this here, so when we go under 30mph, we can fire again
+                this.canDriveBy = true;
+              }
               await Delay(500);
             }
           } else {
+            if (!this.canDriveBy) {
+              SetPlayerCanDoDriveBy(Game.Player.Handle, true); // If we're not in the drivers seat, enable shooting
+              this.canDriveBy = true;
+            }
             await Delay(500);
           }
         } else {
@@ -77,6 +89,11 @@ export class DriveBy {
     if (this.tick !== undefined) {
       clearTick(this.tick);
       this.tick = undefined;
+
+      if (!this.canDriveBy) {
+        SetPlayerCanDoDriveBy(Game.Player.Handle, true); // If we're unarmed, enable shooting
+        this.canDriveBy = true;
+      }
     }
   }
 }
