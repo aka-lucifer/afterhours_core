cf13f961d2015c3bacda35749e1362bd4cab41f8
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-06-12 16:27:12 +0100

    Changed spawner.ts to use spawnmanager.

--- src/client/managers/ui/spawner.ts
+++ src/client/managers/ui/spawner.ts
@@ -43,57 +43,56 @@ export class Spawner {
       this.spawnPosition = new Vector3(aopPosition.x, aopPosition.y, aopPosition.z);
       this.spawnHeading = aopPosition.heading;
 
-      const teleported = await teleportToCoords(this.spawnPosition, this.spawnHeading);
-      if (teleported) {
-        // Set teleport heading and fade screen in
-        Game.PlayerPed.Heading = this.spawnHeading;
-        DoScreenFadeIn(1200);
+      // Get random model from AOP configuration.
+      const randomModel = sharedConfig.aop.spawnModels[Math.floor(Math.random() * sharedConfig.aop.spawnModels.length)];
 
-        // Display character selector UI and enable NUI focus
-        this.client.characters.displayCharacters(false);
-        this.client.richPresence.Text = "Selecting Character";
+      // Spawn into the correct area
+      global.exports["spawnmanager"].spawnPlayer({
+        x: this.spawnPosition.x,
+        y: this.spawnPosition.y,
+        z: this.spawnPosition.z,
+        heading: this.spawnHeading,
+        model: randomModel,
+      }); // Ensure player spawns into server (As we have disabled automatic spawning/respawning).
 
-        // Get random location from AOP positions array
-        const randomModel = sharedConfig.aop.spawnModels[Math.floor(Math.random() * sharedConfig.aop.spawnModels.length)];
-        const spawnModel = new Model(randomModel);
-        const requested = await spawnModel.request(5000);
-        if (requested) {
-          SetPlayerModel(Game.Player.Handle, spawnModel.Hash);
+      DoScreenFadeIn(1200);
 
-          this.spawnBoxZone = new BoxZone({
-            box: {
-              x: this.spawnPosition.x, y: this.spawnPosition.y, z: this.spawnPosition.z, l: 13.2, w: 12.2,
-            },
-            options: {
-              name: "spawn_position",
-              // heading: ,
-              debugPoly: false
-            },
-          }).create();
+      // Display character selector UI and enable NUI focus
+      this.client.characters.displayCharacters(false);
+      this.client.richPresence.Text = "Selecting Character";
 
-          // Add it to our safezone manager
-          // this.client.safezoneManager.add(this.spawnBoxZone);
+      this.spawnBoxZone = new BoxZone({
+        box: {
+          x: this.spawnPosition.x, y: this.spawnPosition.y, z: this.spawnPosition.z, l: 13.2, w: 12.2,
+        },
+        options: {
+          name: "spawn_position",
+          // heading: ,
+          debugPoly: false
+        },
+      }).create();
+
+      // Add it to our safezone manager
+      // this.client.safezoneManager.add(this.spawnBoxZone);
   
-          // this.spawnBoxZone.onPlayerInOut(async(isInside: boolean, pedPos: Vector3) => {
-          //   if (!isInside) {
-          //     // console.log("left spawn zone!, delete from manager and then spawnzone");
-          //     const removed = await this.client.safezoneManager.removeByName(this.spawnBoxZone.options.name);
-          //     if (removed) {
-          //       // console.log("removed from manager!");
-          //       this.spawnBoxZone.destroy();
-          //       // console.log("destroyed spawn poly");
-          //       this.spawnBoxZone = undefined;
-          //       // console.log("set spawn poly to undefined!");
-          //       // Enable PVP in case it didn't enable it after clearing from the manager.
-          //
-          //       SetCanAttackFriendly(Game.PlayerPed.Handle, true, true);
-          //       SetPedSuffersCriticalHits(Game.PlayerPed.Handle, true);
-          //       NetworkSetFriendlyFireOption(true);
-          //     }
-          //   }
-          // });
-        }
-      }
+      // this.spawnBoxZone.onPlayerInOut(async(isInside: boolean, pedPos: Vector3) => {
+      //   if (!isInside) {
+      //     // console.log("left spawn zone!, delete from manager and then spawnzone");
+      //     const removed = await this.client.safezoneManager.removeByName(this.spawnBoxZone.options.name);
+      //     if (removed) {
+      //       // console.log("removed from manager!");
+      //       this.spawnBoxZone.destroy();
+      //       // console.log("destroyed spawn poly");
+      //       this.spawnBoxZone = undefined;
+      //       // console.log("set spawn poly to undefined!");
+      //       // Enable PVP in case it didn't enable it after clearing from the manager.
+      //
+      //       SetCanAttackFriendly(Game.PlayerPed.Handle, true, true);
+      //       SetPedSuffersCriticalHits(Game.PlayerPed.Handle, true);
+      //       NetworkSetFriendlyFireOption(true);
+      //     }
+      //   }
+      // });
       cb("ok");
     });
   }
