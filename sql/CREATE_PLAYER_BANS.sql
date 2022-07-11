75e029da5f20bd7356af533ba47ab8cdb5bf17cf
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-07-11 15:24:24 +0100

    SQL files edits.

--- sql/CREATE_PLAYER_BANS.sql
+++ sql/CREATE_PLAYER_BANS.sql
@@ -3,14 +3,13 @@ CREATE TABLE IF NOT EXISTS `player_bans` (
   `player_id` int(11) NOT NULL,
   `hardware_id` varchar(255) CHARACTER SET latin1 NOT NULL,
   `reason` longtext NOT NULL,
+  `ban_state` int(11) NOT NULL DEFAULT '0',
   `issued_by` int(11) NOT NULL,
-  `issued_on` timestamp NOT NULL DEFAULT current_timestamp(),
-  `issued_until` timestamp NOT NULL DEFAULT current_timestamp(),
+  `issued_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `issued_until` varchar(25) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `player_id` (`player_id`),
-  KEY `hardware_id` (`hardware_id`),
   KEY `issued_by` (`issued_by`),
   FOREIGN KEY (`player_id`) REFERENCES `players` (`player_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (`hardware_id`) REFERENCES `players` (`hardware_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (`issued_by`) REFERENCES `players` (`player_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
