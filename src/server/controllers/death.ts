762829c8d145a9d3b6daefa190664a97cf41d2b7
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-09-05 20:35:02 +0100

    When reviving yourself it now says `You've revived yourself`

--- src/server/controllers/death.ts
+++ src/server/controllers/death.ts
@@ -113,8 +113,12 @@ export class Death {
                     await reviving.TriggerEvent(Events.revive); // Revive player
 
                     // Send revived chat messages
-                    await player.TriggerEvent(Events.sendSystemMessage, new Message(`You have revived ^3${reviving.GetName}^0.`, SystemTypes.Admin));
-                    await reviving.TriggerEvent(Events.sendSystemMessage, new Message(`You have been revived by ^3${player.GetName}^0.`, SystemTypes.Admin));
+                    if (reviving.Handle !== player.Handle) {
+                      await player.TriggerEvent(Events.sendSystemMessage, new Message(`You've revived ^3${reviving.GetName}^0.`, SystemTypes.Admin));
+                      await reviving.TriggerEvent(Events.sendSystemMessage, new Message(`You've been revived by ^3${player.GetName}^0.`, SystemTypes.Admin));
+                    } else {
+                      await player.TriggerEvent(Events.sendSystemMessage, new Message(`You've revived yourself.`, SystemTypes.Admin));
+                    }
                   } else {
                     await player.TriggerEvent(Events.sendSystemMessage, new Message("This player isn't dead!", SystemTypes.Error));
                   }
