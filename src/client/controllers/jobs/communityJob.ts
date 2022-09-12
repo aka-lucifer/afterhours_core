c23bed59ec5cd2f92d904c2abe05875235fe5d49
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2022-09-12 20:22:48 +0100

    COfficer now has weapons & join RTO buttons in menu.

--- src/client/controllers/jobs/communityJob.ts
+++ src/client/controllers/jobs/communityJob.ts
@@ -1,14 +1,21 @@
-import { Game, Model } from 'fivem-js';
+import {Game, Model} from 'fivem-js';
 
-import { Client } from '../../client';
-import { Inform } from '../../utils';
+import {Client} from '../../client';
+import {Inform} from '../../utils';
 
-import { Menu } from '../../models/ui/menu/menu';
-import { MenuPositions } from '../../../shared/enums/ui/menu/positions';
-import { JobEvents } from '../../../shared/enums/events/jobs/jobEvents';
-import { Jobs } from '../../../shared/enums/jobs/jobs';
-import { Events } from '../../../shared/enums/events/events';
-import { JobCallbacks } from '../../../shared/enums/events/jobs/jobCallbacks';
+import {Menu} from '../../models/ui/menu/menu';
+import {Submenu} from "../../models/ui/menu/submenu";
+
+import {MenuPositions} from '../../../shared/enums/ui/menu/positions';
+import {JobEvents} from '../../../shared/enums/events/jobs/jobEvents';
+import {Jobs} from '../../../shared/enums/jobs/jobs';
+import {Events} from '../../../shared/enums/events/events';
+import {JobCallbacks} from '../../../shared/enums/events/jobs/jobCallbacks';
+
+import sharedConfig from "../../../configs/shared.json";
+import {Weapons} from "../../../shared/enums/weapons";
+import {Notification} from "../../models/ui/notification";
+import {NotificationTypes} from "../../../shared/enums/ui/notifications/types";
 
 export class CommunityJob {
   private client: Client;
@@ -16,8 +23,14 @@ export class CommunityJob {
   // Menus
   private menuBuilt: boolean = false;
   private menu: Menu;
+  private weapons: Submenu;
 
   // Menu Data
+  private rifle: string;
+  private shotgun: string;
+  private pistol: string;
+  private tazer: string;
+  private nightstick: string;
 
   constructor(client: Client) {
     this.client = client;
@@ -50,37 +63,42 @@ export class CommunityJob {
     if (this.client.Character.job.name === Jobs.Community) {
       this.menu.BindCheckbox("On Duty", this.client.Character.job.status, (newState: boolean) => {
         this.client.cbManager.TriggerServerCallback(JobCallbacks.setDuty, async(returnedState: boolean) => {
+          this.client.Character.job.status = returnedState;
+
           if (returnedState) {
-            this.client.Character.job.status = newState;
+            const currModel = this.client.modelBlacklist.Model;
 
-            if (newState) {
-              const currModel = this.client.modelBlacklist.Model;
+            // If your current ped model isn't the `cofficer` ped model
+            if (currModel.Hash !== GetHashKey("cofficer")) {
+              const newModel = new Model("cofficer");
+              const modelLoaded = await newModel.request(2000); // Load the ped into memory, if it isn't loaded yet
+              if (modelLoaded) { // Once it has been loaded, apply the model to your player and then remove the model from memory
+                SetPlayerModel(Game.Player.Handle, newModel.Hash); // Apply the model to your player
+                this.client.modelBlacklist.Model = newModel; // Set this model to your new model in the model blacklist controller
+                newModel.markAsNoLongerNeeded(); // Remove the model from memory
 
-              // If your current ped model isn't the `cofficer` ped model
-              if (currModel.Hash !== GetHashKey("cofficer")) {
-                const newModel = new Model("cofficer");
-                const modelLoaded = await newModel.request(2000); // Load the ped into memory, if it isn't loaded yet
-                if (modelLoaded) { // Once it has been loaded, apply the model to your player and then remove the model from memory
-                  SetPlayerModel(Game.Player.Handle, newModel.Hash); // Apply the model to your player
-                  this.client.modelBlacklist.Model = newModel; // Set this model to your new model in the model blacklist controller
-                  newModel.markAsNoLongerNeeded(); // Remove the model from memory
+                // Sets styling of ped
+                SetPedComponentVariation(Game.PlayerPed.Handle, 0, 1, 1, 0);
+                SetPedComponentVariation(Game.PlayerPed.Handle, 1, 0, 0, 0);
+                SetPedComponentVariation(Game.PlayerPed.Handle, 2, 0, 0, 0);
+                SetPedComponentVariation(Game.PlayerPed.Handle, 3, 1, 0, 0);
+                SetPedComponentVariation(Game.PlayerPed.Handle, 4, 0, 0, 0);
+                SetPedComponentVariation(Game.PlayerPed.Handle, 8, 2, 0, 0);
 
-                  // Sets styling of ped
-                  SetPedComponentVariation(Game.PlayerPed.Handle, 0, 1, 1, 0);
-                  SetPedComponentVariation(Game.PlayerPed.Handle, 1, 0, 0, 0);
-                  SetPedComponentVariation(Game.PlayerPed.Handle, 2, 0, 0, 0);
-                  SetPedComponentVariation(Game.PlayerPed.Handle, 3, 1, 0, 0);
-                  SetPedComponentVariation(Game.PlayerPed.Handle, 4, 0, 0, 0);
-                  SetPedComponentVariation(Game.PlayerPed.Handle, 8, 2, 0, 0);
-                }
+                // Set weapon menu checkboxes, as setting a new model on your ped removes your weapons
+                this.client.menuManager.UpdateState(this.rifle, false);
+                this.client.menuManager.UpdateState(this.shotgun, false);
+                this.client.menuManager.UpdateState(this.pistol, false);
+                this.client.menuManager.UpdateState(this.tazer, false);
+                this.client.menuManager.UpdateState(this.nightstick, false);
               }
-
-              global.exports["pma-voice"].setVoiceProperty("radioEnabled", true);
-              global.exports["pma-voice"].setRadioChannel(245.1, "LEO (Main RTO)");
-            } else {
-              global.exports["pma-voice"].setVoiceProperty("radioEnabled", false);
-              global.exports["pma-voice"].setRadioChannel(0);
             }
+
+            global.exports["pma-voice"].setVoiceProperty("radioEnabled", true);
+            global.exports["pma-voice"].setRadioChannel(245.1, "LEO (Main RTO)");
+          } else {
+            global.exports["pma-voice"].setVoiceProperty("radioEnabled", false);
+            global.exports["pma-voice"].setRadioChannel(0);
           }
         }, newState);
       })
@@ -103,6 +121,68 @@ export class CommunityJob {
 
       this.menu.BindButton("Unseat Player", () => {
         emitNet(Events.unseatPlayer);
+      });
+
+      // WEAPONS
+      this.weapons = this.menu.BindSubmenu("Weapons");
+
+      this.rifle = this.weapons.BindCheckbox(sharedConfig.weapons[Weapons.AR15].label, HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.AR15, false), (newState: boolean) => {
+        this.client.menuManager.UpdateState(this.rifle, newState);
+        if (!HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.AR15, false)) {
+          const [_, maxAmmo] = GetMaxAmmo(Game.PlayerPed.Handle, Weapons.AR15);
+          Game.PlayerPed.giveWeapon(Weapons.AR15, maxAmmo, false, true);
+        } else {
+          RemoveWeaponFromPed(Game.PlayerPed.Handle, Weapons.AR15);
+        }
+      });
+
+      this.shotgun = this.weapons.BindCheckbox(sharedConfig.weapons[Weapons.Remington870].label, HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.Remington870, false), (newState: boolean) => {
+        this.client.menuManager.UpdateState(this.shotgun, newState);
+        if (!HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.Remington870, false)) {
+          const [_, maxAmmo] = GetMaxAmmo(Game.PlayerPed.Handle, Weapons.Remington870);
+          Game.PlayerPed.giveWeapon(Weapons.Remington870, maxAmmo, false, true);
+        } else {
+          RemoveWeaponFromPed(Game.PlayerPed.Handle, Weapons.Remington870);
+        }
+      });
+
+      this.pistol = this.weapons.BindCheckbox(sharedConfig.weapons[Weapons.Glock17].label, HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.Glock17, false), (newState: boolean) => {
+        this.client.menuManager.UpdateState(this.pistol, newState);
+        if (!HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.Glock17, false)) {
+          const [_, maxAmmo] = GetMaxAmmo(Game.PlayerPed.Handle, Weapons.Glock17);
+          Game.PlayerPed.giveWeapon(Weapons.Glock17, maxAmmo, false, true);
+        } else {
+          RemoveWeaponFromPed(Game.PlayerPed.Handle, Weapons.Glock17);
+        }
+      });
+
+      this.tazer = this.weapons.BindCheckbox(sharedConfig.weapons[Weapons.X26Tazer].label, HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.X26Tazer, false), (newState: boolean) => {
+        this.client.menuManager.UpdateState(this.tazer, newState);
+        if (!HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.X26Tazer, false)) {
+          const [_, maxAmmo] = GetMaxAmmo(Game.PlayerPed.Handle, Weapons.X26Tazer);
+          Game.PlayerPed.giveWeapon(Weapons.X26Tazer, maxAmmo, false, true);
+        } else {
+          RemoveWeaponFromPed(Game.PlayerPed.Handle, Weapons.X26Tazer);
+        }
+      });
+
+      this.nightstick = this.weapons.BindCheckbox(sharedConfig.weapons[Weapons.Nightstick].label, HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.Nightstick, false), (newState: boolean) => {
+        this.client.menuManager.UpdateState(this.nightstick, newState);
+        if (!HasPedGotWeapon(Game.PlayerPed.Handle, Weapons.Nightstick, false)) {
+          Game.PlayerPed.giveWeapon(Weapons.Nightstick, 0, false, true);
+        } else {
+          RemoveWeaponFromPed(Game.PlayerPed.Handle, Weapons.Nightstick);
+        }
+      });
+
+      this.menu.BindButton("Join RTO", async() => {
+        if (this.client.Character.Job.status) { // If on duty
+          global.exports["pma-voice"].setVoiceProperty("radioEnabled", true);
+          global.exports["pma-voice"].setRadioChannel(245.1, "LEO (Main RTO)");
+        } else {
+          const notify = new Notification("Radio", "You aren't on duty!", NotificationTypes.Error);
+          await notify.send();
+        }
       });
 
       if (!this.menuBuilt) this.menuBuilt = true;
